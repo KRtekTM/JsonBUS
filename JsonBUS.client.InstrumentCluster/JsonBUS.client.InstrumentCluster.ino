5724627aed9a48b1bd7c9217eed9023867913322
Ondřej Kotas <ok@krtkovo.eu>
2020-04-23 04:52:41 +0200

    alpha

--- JsonBUS.client.InstrumentCluster/JsonBUS.client.InstrumentCluster.ino
+++ JsonBUS.client.InstrumentCluster/JsonBUS.client.InstrumentCluster.ino
@@ -10,34 +10,39 @@
 //const SwitecX25 gauge_speed(STEPS, 6,7,8,9); //uses 4 digital pins, 315*3=945 steps
 //const SwitecX25 gauge_temp(STEPS, 10,11,12,13); //uses 4 digital pins, 315*3=945 steps
 //const SwitecX25 gauge_gas(STEPS, 14,15,16,17); //uses 4 digital pins, 315*3=945 steps
-const int cluster_light = 0;
-const int left_indicator = 0;
-const int right_indicator = 0;
+const int cluster_light = 11;
+const int left_indicator = 22;
+const int right_indicator = 32;
 //const int both_indicators = 0; // for connecting HW button light on dashboard
-const int airbag = 53;
-const int light_lowbeam = 24;
-const int light_mainbeam = 36;
-const int light_fogFront = 0;
-const int light_fogRear = 0;
-const int warn_temp = 49;
-const int warn_gas = 0;
-const int warn_oil = 51;
+const int airbag = 23;
+const int light_lowbeam = 25;
+const int light_mainbeam = 27;
+const int light_fogFront = 24;
+const int light_fogRear = 26;
+const int warn_temp = 29;
+const int warn_gas = 31;
+const int warn_oil = 28;
 const int warn_engine = 33;
-const int warn_failure = 0;
-const int warn_abs = 0;
-const int warn_immobilizer = 0;
-const int warn_handbrake = 0;
-const int warn_battery = 31;
-const int cruisecontrol = 32;
-const int speaker = 11;
+const int warn_failure = 34;
+const int warn_abs = 35;
+const int warn_immobilizer = 36;
+const int warn_handbrake = 37;
+const int warn_battery = 38;
+const int cruisecontrol = 30;
+const int speaker = 42;
+const int reservePin0 = 39;
+const int reservePin1 = 40;
+const int reservePin2 = 41;
 
 const int noteFrequency = 4500;
 
-int state_electricity = 0;
+int state_electricity, state_blinker = 0;
+bool state_gasLow = false;
 
 // Celebration is typical Skoda/Volkswagen instrument cluster lightshow when electricity is on
 void clusterCelebration() {
   // Ignition switch position 1
+  analogWrite(cluster_light, 10);
   digitalWrite(airbag, HIGH);
   digitalWrite(warn_temp, HIGH);
   digitalWrite(warn_gas, HIGH);
@@ -60,6 +65,7 @@ void clusterCelebration() {
   
   //gauge_rpm.setPosition(0);
   //gauge_speed.setPosition(0);
+  digitalWrite(warn_gas, LOW);
 
   delay(500);
   
@@ -96,10 +102,7 @@ void clusterCelebration() {
   digitalWrite(warn_immobilizer, LOW);
   noTone(speaker);
 
-  delay(250);
-  digitalWrite(warn_gas, LOW);
-
-  delay(750);
+  delay(1000);
   digitalWrite(warn_immobilizer, HIGH);
   tone(speaker, noteFrequency);
 
@@ -136,7 +139,7 @@ void clusterTurnOff() {
 }
 
 void testLights() {
-  digitalWrite(cluster_light, HIGH);
+  analogWrite(cluster_light, 100);
   digitalWrite(left_indicator, HIGH);
   digitalWrite(right_indicator, HIGH);
   digitalWrite(airbag, HIGH);
@@ -159,60 +162,61 @@ void testLights() {
 
   clusterTurnOff();
 
-  digitalWrite(cluster_light, HIGH);
-  delay(500);
-  digitalWrite(cluster_light, LOW);
+  analogWrite(cluster_light, 100);
+  delay(300);
+  analogWrite(cluster_light, 0);
   digitalWrite(left_indicator, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(left_indicator, LOW);
   digitalWrite(right_indicator, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(right_indicator, LOW);
   digitalWrite(airbag, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(airbag, LOW);
   digitalWrite(light_lowbeam, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(light_lowbeam, LOW);
-  digitalWrite(light_mainbeam, HIGH);
-  delay(500);
-  digitalWrite(light_mainbeam, LOW);
   digitalWrite(light_fogFront, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(light_fogFront, LOW);
   digitalWrite(light_fogRear, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(light_fogRear, LOW);
+  digitalWrite(light_mainbeam, HIGH);
+  delay(300);
+  digitalWrite(light_mainbeam, LOW);
   digitalWrite(warn_temp, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_temp, LOW);
+  digitalWrite(cruisecontrol, HIGH);
+  delay(300);
+  digitalWrite(cruisecontrol, LOW);
   digitalWrite(warn_gas, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_gas, LOW);
   digitalWrite(warn_oil, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_oil, LOW);
   digitalWrite(warn_engine, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_engine, LOW);
   digitalWrite(warn_failure, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_failure, LOW);
   digitalWrite(warn_abs, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_abs, LOW);
   digitalWrite(warn_immobilizer, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_immobilizer, LOW);
   digitalWrite(warn_handbrake, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_handbrake, LOW);
   digitalWrite(warn_battery, HIGH);
-  delay(500);
+  delay(300);
   digitalWrite(warn_battery, LOW);
-  digitalWrite(cruisecontrol, HIGH);
-  delay(500);
-  digitalWrite(cruisecontrol, LOW);
+
 
   delay(500);
 
@@ -263,18 +267,32 @@ void busCommunication() {
 
     // engine on/off
     case 231:
-      (featureValue == 1) ? digitalWrite(warn_battery, LOW) : digitalWrite(warn_battery, HIGH);
+      if(state_electricity != 0) {
+        if (featureValue == 1) {
+          digitalWrite(warn_battery, LOW);
+        }
+        else {
+          digitalWrite(warn_battery, HIGH);
+        }
+      }
       break;
 
     // cruisecontrol on/off
     case 232:
-      (featureValue == 1) ? digitalWrite(cruisecontrol, HIGH) : digitalWrite(cruisecontrol, LOW);
+      if(state_electricity == 1) { // turn on only when electricity is on
+        (featureValue == 1) ? digitalWrite(cruisecontrol, HIGH) : digitalWrite(cruisecontrol, LOW);
+      }
       break;
 
     // temp
     case 233:
       // turn the control on when temp is above 120°C
-      (featureValue >= 120) ? digitalWrite(warn_temp, HIGH) : digitalWrite(warn_temp, LOW);
+      if (featureValue >= 120) {
+        digitalWrite(warn_temp, HIGH);
+      }
+      else {
+        digitalWrite(warn_temp, LOW);
+      }
 
       // control the gauge (MIN=50°C,MAX=130°C)
       if (featureValue <= 50) featureValue = 50;
@@ -284,8 +302,13 @@ void busCommunication() {
       
     // gas level
     case 234:
-      // turn the control on when gas level is below 5%
-      (featureValue <= 5) ? digitalWrite(warn_gas, HIGH) : digitalWrite(warn_gas, LOW);
+      // turn the control on when gas level is below 10%
+      if (featureValue <= 15) {
+        digitalWrite(warn_gas, HIGH);
+      }
+      else {
+        digitalWrite(warn_gas, LOW);
+      }
 
       // control the gauge (MIN=5%,MAX=100%)
       if (featureValue <= 5) featureValue = 5;
@@ -312,7 +335,14 @@ void busCommunication() {
     // low light beam on/off
     case 237:
       if(state_electricity == 1) { // turn on only when electricity is on
-        (featureValue == 1) ? digitalWrite(light_lowbeam, HIGH) : digitalWrite(light_lowbeam, LOW);
+        if (featureValue == 1) {
+          digitalWrite(light_lowbeam, HIGH);
+          //analogWrite(cluster_light, 100);
+        }
+        else {
+          digitalWrite(light_lowbeam, LOW);
+          //analogWrite(cluster_light, 0);
+        }
       }
       break;
 
@@ -323,7 +353,9 @@ void busCommunication() {
 
     // cluster backlight on/off
     case 239:
-      (featureValue == 1) ? digitalWrite(cluster_light, HIGH) : digitalWrite(cluster_light, LOW);
+      if(state_electricity == 1) { // turn on only when electricity is on
+        analogWrite(cluster_light, featureValue);
+      }
       break;
 
     // indicators on/off
@@ -332,22 +364,38 @@ void busCommunication() {
         case 1:
           if(state_electricity == 1) { // turn on only when electricity is on
             digitalWrite(left_indicator, HIGH);
-            digitalWrite(right_indicator, LOW);  
+            digitalWrite(right_indicator, LOW);
+            if(state_blinker == 0) {
+              tone(speaker, 500, 10);
+              state_blinker = 1;
+            }
           }
           break;
         case 2:
           if(state_electricity == 1) { // turn on only when electricity is on
             digitalWrite(left_indicator, LOW);
             digitalWrite(right_indicator, HIGH);
+            if(state_blinker == 0) {
+              tone(speaker, 500, 10);
+              state_blinker = 1;
+            }
           }
           break;
         case 3:
           digitalWrite(left_indicator, HIGH);
           digitalWrite(right_indicator, HIGH);
+            if(state_blinker == 0) {
+              tone(speaker, 500, 10);
+              state_blinker = 1;
+            }
           break;
         default:
           digitalWrite(left_indicator, LOW);
           digitalWrite(right_indicator, LOW);
+          if(state_blinker == 1) {
+            tone(speaker, 300, 10);
+            state_blinker = 0;  
+          }
           break;
       }
       break;
@@ -368,7 +416,9 @@ void busCommunication() {
 
     // parking brake light on/off
     case 244:
-      (featureValue == 1) ? digitalWrite(warn_handbrake, HIGH) : digitalWrite(warn_handbrake, LOW);
+      if(state_electricity == 1) { // turn on only when electricity is on
+        (featureValue == 1) ? digitalWrite(warn_handbrake, HIGH) : digitalWrite(warn_handbrake, LOW);
+      }
       break;
 
     // motor brake light on/off
@@ -381,12 +431,11 @@ void busCommunication() {
     // engine damage report
     case 246:
       if(state_electricity == 1) { // turn on only when electricity is on
-        (featureValue >= 30) ? digitalWrite(warn_engine, HIGH) : digitalWrite(warn_engine, LOW);
-        (featureValue >= 35) ? digitalWrite(warn_failure, HIGH) : digitalWrite(warn_failure, LOW);
-        (featureValue >= 40) ? digitalWrite(warn_oil, HIGH) : digitalWrite(warn_oil, LOW);
-        (featureValue >= 45) ? digitalWrite(warn_abs, HIGH) : digitalWrite(warn_abs, LOW);
-        (featureValue >= 50) ? digitalWrite(warn_temp, HIGH) : digitalWrite(warn_temp, LOW);
-        (featureValue >= 55) ? digitalWrite(airbag, HIGH) : digitalWrite(airbag, LOW);
+        (featureValue >= 20) ? digitalWrite(warn_engine, HIGH) : digitalWrite(warn_engine, LOW);
+        (featureValue >= 25) ? digitalWrite(warn_failure, HIGH) : digitalWrite(warn_failure, LOW);
+        (featureValue >= 30) ? digitalWrite(warn_oil, HIGH) : digitalWrite(warn_oil, LOW);
+        (featureValue >= 40) ? digitalWrite(warn_temp, HIGH) : digitalWrite(warn_temp, LOW);
+        (featureValue >= 65) ? digitalWrite(airbag, HIGH) : digitalWrite(airbag, LOW);
       }     
       break;
 
---
+++ JsonBUS.manager.ETS2/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Windows.Forms;
+using SCSSdkClient;
+using SCSSdkClient.Object;
+using System.Threading;
+
+namespace JsonBUS.manager.ETS2
+{
+    public partial class Form1 : Form
+    {
+        public SCSSdkTelemetry Telemetry;
+        public SerialPort Port;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //foreach(String port in SerialPort.GetPortNames())
+            //{
+            //    listView1.Items.Add(port);
+            //}
+
+            Port = new SerialPort("COM8", 115200);
+
+            Telemetry = new SCSSdkTelemetry();
+            Telemetry.Data += Telemetry_Data;
+
+        }
+
+
+        private bool state_electricity = false;
+
+        private void Telemetry_Data(SCSTelemetry data, bool updated)
+        {
+            try
+            {
+                // Invoke telemetry collection
+                if (InvokeRequired)
+                {
+                    Invoke(new TelemetryData(Telemetry_Data), data, updated);
+                    return;
+                }
+                
+                // Open Serial port to communicate with JsonBUS client
+                if (!Port.IsOpen)
+                {
+                    Port.Open();
+                }
+
+                if (data.SdkActive && !data.Paused)
+                {
+
+
+                    // Blinkers
+                    if (data.TruckValues.CurrentValues.LightsValues.BlinkerLeftOn == true && data.TruckValues.CurrentValues.LightsValues.BlinkerRightOn == false) Port.WriteLine("{\"f\":240,\"v\":1}");
+                    else if (data.TruckValues.CurrentValues.LightsValues.BlinkerLeftOn == false && data.TruckValues.CurrentValues.LightsValues.BlinkerRightOn == true) Port.WriteLine("{\"f\":240,\"v\":2}");
+                    else if (data.TruckValues.CurrentValues.LightsValues.BlinkerLeftOn == true && data.TruckValues.CurrentValues.LightsValues.BlinkerRightOn == true) Port.WriteLine("{\"f\":240,\"v\":3}");
+                    else Port.WriteLine("{\"f\":240,\"v\":0}");
+
+                    //Thread.Sleep(1);
+
+                    // Electric enabled
+                    if (state_electricity != data.TruckValues.CurrentValues.ElectricEnabled)
+                    {
+                        if (data.TruckValues.CurrentValues.ElectricEnabled)
+                        {
+                            Port.WriteLine("{\"f\":230,\"v\":1}");
+                        }
+                        else
+                        {
+                            Port.WriteLine("{\"f\":230,\"v\":0}");
+                        }
+                        state_electricity = data.TruckValues.CurrentValues.ElectricEnabled;
+                    }
+
+                    //Thread.Sleep(1);
+
+                    // Engine enabled
+                    if (data.TruckValues.CurrentValues.EngineEnabled)
+                    {
+                        Port.WriteLine("{\"f\":231,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":231,\"v\":0}");
+                    }
+
+                    //Thread.Sleep(1);
+
+                    // Damage report
+                    int damage = (int)Math.Ceiling(data.TruckValues.CurrentValues.DamageValues.Chassis * 100);
+                    Port.WriteLine("{\"f\":246,\"v\":" + damage + "}");
+                    //Thread.Sleep(1);
+
+                    // Lights
+                    if (data.TruckValues.CurrentValues.LightsValues.Parking)
+                    {
+                        Port.WriteLine("{\"f\":239,\"v\":" + (data.TruckValues.CurrentValues.LightsValues.DashboardBacklight * 100) + "}");
+                    }
+
+                    if (data.TruckValues.CurrentValues.LightsValues.BeamLow)
+                    {
+                        Port.WriteLine("{\"f\":237,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":237,\"v\":0}");
+                    }
+                    //Thread.Sleep(1);
+
+                    if (data.TruckValues.CurrentValues.LightsValues.BeamHigh || data.TruckValues.CurrentValues.LightsValues.AuxFront == AuxLevel.Full)
+                    {
+                        Port.WriteLine("{\"f\":238,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":238,\"v\":0}");
+                    }
+                    //Thread.Sleep(1);
+
+                    if (data.TruckValues.CurrentValues.MotorValues.BrakeValues.ParkingBrake)
+                    {
+                        Port.WriteLine("{\"f\":244,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":244,\"v\":0}");
+                    }
+                    //Thread.Sleep(1);
+
+                    if (data.TruckValues.CurrentValues.LightsValues.Brake && !data.TruckValues.CurrentValues.MotorValues.BrakeValues.ParkingBrake)
+                    {
+                        Port.WriteLine("{\"f\":245,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":245,\"v\":0}");
+                    }
+                    //Thread.Sleep(1);
+
+                    if (data.TruckValues.CurrentValues.DashboardValues.CruiseControl)
+                    {
+                        Port.WriteLine("{\"f\":232,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":232,\"v\":0}");
+                    }
+
+                    if(data.TrailerValues[0].Attached)
+                    {
+                        Port.WriteLine("{\"f\":241,\"v\":1}");
+                    }
+                    else
+                    {
+                        Port.WriteLine("{\"f\":241,\"v\":0}");
+                    }
+
+                    // GAUGES
+                    // Gas
+                    var gasLevelPercentage = Math.Ceiling((data.TruckValues.CurrentValues.DashboardValues.FuelValue.Amount / data.TruckValues.ConstantsValues.CapacityValues.Fuel) * 100);
+                    Port.WriteLine("{\"f\":234,\"v\":" + gasLevelPercentage + "}");
+                    //Thread.Sleep(1);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                // ignored atm i found no proper way to shut the telemetry down and down call this anymore when this or another thing is already disposed
+                Console.WriteLine("Telemetry was closed: " + ex);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Thread.Sleep(10);
+            Port.WriteLine("{\"f\":230,\"v\":0}");
+        }
+    }
+}
